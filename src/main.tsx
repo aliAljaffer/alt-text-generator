af0535efe917d3ff1ed59587b0b12c65d62a9fe2
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2025-06-24 20:29:07 +0300

    Redirect URI updated for hopefully the last time

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ const cognitoAuthConfig = {
   authority:
     "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_IJYxTZ6jm",
   client_id: "2cbc1hd3l8hm3js05j18t3f8ok",
-  redirect_uri: "https://d1qvraodbtp7zs.cloudfront.net/",
+  redirect_uri: "https://textify.alialjaffer.com/",
   response_type: "code",
   scope: "phone openid email",
 };
